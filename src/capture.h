adf1fbeadff5fceb450f29d46934c2941469426e
victorcharlie <minhlong1312@gmail.com>
2020-11-22 04:09:42 -0800

    Move functions decleration to headers

--- src/capture.h
+++ src/capture.h
@@ -1,5 +1,16 @@
 #ifndef CAPTURE_HEADER
 #define CAPTURE_HEADER
-#include <packet.h>
+#include "packet.h"
+#include <pfring.h>
+
+void pfring_packet_process();
 void pfring_capture(packet_process_ptr func_ptr);
+void init_logging();
+void parsing_pfring_packet(const struct pfring_pkthdr* h, const u_char* p, const u_char* user_bytes);
+void start_pfring_capture();
+void stop_pfring_capture();
+
+bool start_pfring_packet_preprocessing(const char* dev);
+
+
 #endif
--- src/process_packet.h
+++ src/process_packet.h
@@ -1,7 +1,14 @@
 #ifndef PROCESS_PACKET_HEADER
 #define PROCESS_PACKET_HEADER
+#include <string>
 #include "packet.h"
-#include "string"
+
 using namespace std;
-void process_packet(packet& current_packet, string packet_file);
+
+void process_packet(packet &current_packet, string packet_file);
+
+string ip_int_to_string(uint32_t ip_as_integer);
+
+string log_packet(packet packet, string packet_file);
+
 #endif
