bcde794166c849462604152a5cbb8b08c275d685
sanebinary <anh.nguyenduc259wk@gmail.com>
2020-11-29 04:39:15 -0500

    remove unnecessary lib logger4cpp

--- src/fireflow.cpp
+++ src/fireflow.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <CLI11.hpp>
+#include "CLI11.hpp"
 #include "capture.h"
 #include "packet.h"
 using namespace std;
@@ -10,7 +10,7 @@ int packet::internalPacketCounter = 0;
 int main(int argc, char* argv[]){
     CLI::App app{"FIREFLOW: USTH ANTI_DDOS"};
 
-    std::string interface, logfile_path, packetfile_path;
+    string interface, logfile_path, packetfile_path;
     app.add_option("-i,--interface", interface, "Capture interface")->required();
     app.add_option("-l", logfile_path, "Dump to log file");
     app.add_option("-p", packetfile_path,  "Where to dump packet log");
@@ -19,5 +19,4 @@ int main(int argc, char* argv[]){
     Capture capture_inteface(interface, logfile_path, packetfile_path);
     capture_inteface.init_logging();
     capture_inteface.start_pfring_capture();
-    
 }
