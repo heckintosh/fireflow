77317999be6ac4af879fbba5d9bbaa4370381153
0xF1ash <anh.nguyenduc259@gmail.com>
2021-01-18 00:45:51 +0700

    estimating target value for cusum chart

--- src/detection.cpp
+++ src/detection.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <sstream>
 #include <algorithm>
 #include <vector>
-#include <map>
 #include <numeric>
 #include <math.h>
 
@@ -17,17 +14,32 @@ double calcMean(vector<double> tmp_data)
     return mean;
 }
 
-double standardDeviation(vector<double> tmp_data, double mean)
-{
-    std::vector<double> diff(tmp_data.size());
-    std::transform(tmp_data.begin(), tmp_data.end(), diff.begin(), [mean](double x) { return x - mean; });
-    double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
-    double stdev = std::sqrt(sq_sum / tmp_data.size());
-    return stdev;
+double targetValue(vector<double> entropies, double subwindow, double window){
+    double size = entropies.size();
+    uint total_entropies_one_window = window / subwindow; 
+    uint index = 0;
+    double tmp_total = 0;
+    vector<double> windows_means;
+    vector<double> subwindow_elems;
+    for (int index = 0; index < size; ++index){
+        if (index % total_entropies_one_window != 0 && index == 0){
+            subwindow_elems.push_back(entropies[index]);
+        }
+        else{
+            windows_means.push_back(calcMean(subwindow_elems));
+            subwindow_elems.clear();
+        }
+    }
+    double value = calcMean(windows_means);
+    return value;
 }
 
-//given a vector of the average entropies in multiple windows
-// output the grand mean of those as target Value for CUSUM
-double targetValue(vector<double> means_window){
-    return calcMean(means_window);
+double sampleStdev(vector<double> tmp_data)
+{
+    double mean = calcMean(tmp_data);
+    vector<double> diff(tmp_data.size());
+    transform(tmp_data.begin(), tmp_data.end(), diff.begin(), [mean](double x) { return x - mean; });
+    double sq_sum = inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+    double stdev = sqrt(sq_sum / (tmp_data.size() - 1));
+    return stdev;
 }
